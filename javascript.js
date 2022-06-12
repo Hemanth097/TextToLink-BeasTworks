56375538f91ce5dd6a1f80c0f3f4aa52403f3e12
Hemanth097 <64942691+Hemanth097@users.noreply.github.com>
2022-06-12 12:33:13 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,19 +1,19 @@
 function GenerateURL() {
 
 	var texturl = encodeURIComponent(document.getElementById("matter").value);
-	var comurl = "file:///C:/Users/sys2/Desktop/hemanth/Replies/reply.html?name=" + texturl + "";
+	var comurl = "https://hemanth097.github.io/TextToLink-BeasTworks/reply.html?name=" + texturl + "";
 
 	document.getElementById("urcode").innerHTML = "Generated url : <a href = " + comurl + " > "+ comurl + "</a>";
-    //window.open("https://wa.me/91" + document.getElementById("wno").value + "?text=" + texturl + "", "_blank");
+
 
 }
 
 function openLink() {
 	
 	var texturl = encodeURIComponent(document.getElementById("matter").value);
-	var comurl = "file:///C:/Users/sys2/Desktop/hemanth/Replies/reply.html?name=" + texturl + "";
+	var comurl = "https://hemanth097.github.io/TextToLink-BeasTworks/reply.html?name=" + texturl + "";
 
 	//document.getElementById("urcode").innerHTML = "Generated url : <a href = " + comurl + " > "+ comurl + "</a>";
-    window.open("file:///C:/Users/sys2/Desktop/hemanth/Replies/reply.html?name=" + texturl + "", "_blank");
+    window.open("https://hemanth097.github.io/TextToLink-BeasTworks/reply.html?name=" + texturl + "", "_blank");
 
 }
